8f7120c623cb0161604d00236ae5cdb15aebaa39
Iván Ovejero <ivov.src@gmail.com>
2021-10-10 13:19:54 +0200

    Adjust path to workflows

--- gen/tests/index.js
+++ gen/tests/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { main } = require("..");
 
 (async () => {
-  const mocksDir = path.resolve(__dirname, "..", "..", "workflows");
+  const mocksDir = path.resolve(__dirname, "..", "..", "trash", "workflows");
   const filesAtDir = await fs.promises.readdir(mocksDir);
   filesAtDir.forEach(main);
 })();
